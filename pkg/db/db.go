b72f5e1d4f845e638dacf18ba80fb5254ee48c1d
Vadym Sorokin <nicourrrn@gmail.com>
2022-01-14 00:20:00 +0200

    Add db connection

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,7 +1,21 @@
 package db
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"os"
+	"time"
 )
 
-type Connection *sqlx.DB
+const CacheLifeTime = time.Minute
+
+type Connection sqlx.DB
+
+func NewConnection() (*Connection, error){
+	db, err := sqlx.Connect("mysql", os.Getenv("DB_NAME")+"@'localhost'")
+	if err != nil {
+		return nil, err
+	}
+	return (*Connection)(db), nil
+}
+
